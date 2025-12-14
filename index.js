c3de6f86dfff3a59d882421c5d54e90ac9520454
808MbullNotFound <110455523+NaufalNyaa@users.noreply.github.com>
2025-12-15 02:53:35 +0700

    cinta satu malam

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
+import jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git";
+import random from "random";
 
+const path = "./data.json";
+
+const markCommit = (x, y) => {
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = {
+    date: date,
+  };
+
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date }).push();
+  });
+};
+
+const makeCommits = (n) => {
+  if(n === 0) return simpleGit().push();
+
+  const x = random.int(0, 54);
+  const y = random.int(0, 6);
+  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+
+  const data = {
+    date: date,
+  };
+
+  console.log(date);
+
+  jsonfile.writeFile(path, data, (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      return;
+    }
+    simpleGit().add([path]).commit(date, { "--date": date }, () => {
+      makeCommits(n - 1);
+    });
+  });
+};
+
+makeCommits(500);
